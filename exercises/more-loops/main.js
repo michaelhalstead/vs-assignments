87b37b4e782fce78787abc449208d901f75e0176
Michael <michael@redmanedigital.com>
2018-07-20 11:09:48 -0600

    blah

---
+++ exercises/more-loops/main.js
@@ -0,0 +1,18 @@
+var names = [
+    {
+        firstName: "John",
+        lastName: "Wayne",
+        age: 67,
+        email: "hesdead@noemail.com"
+    },
+    {
+        firstName: "Jennifer",
+        lastName: "Lawrence",
+        age: 26,
+        email: "sexyjen@gmail.com"
+    },
+];
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i].firstName)
+}
